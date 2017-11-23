61ca78af59378b869a5e7915059d151c265761d8
mdiop <diop.mamadou-pro@laposte.net>
2017-11-23 15:51:44 +0100

    # display images

---
+++ HTTP/exercice1-partie1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<strong>It's worked !!!</strong>
+<p>Ceci est une petite page de test ! :) </p>
+<h2>les images </h2>
+</body>
+</html>
