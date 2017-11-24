5c8ddec922a5cf56ca2b8e7632cd72982f5869cb
mdiop <diop.mamadou-pro@laposte.net>
2017-11-24 17:15:36 +0100

    #add Calculatrice

---
+++ POO/Entity/Calculateur.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Franck
+ * Date: 24/11/2017
+ * Time: 17:13
+ */
+
+class Calculateur
+{
+
+
+    public static function Addition(){
+
+    }
+    public static function Soustraction(){
+
+    }
+    public static function Multiplication(){
+
+    }
+    public static function RestedeDivione(){
+
+    }
+}
