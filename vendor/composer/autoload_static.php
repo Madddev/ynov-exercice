ac6022f101fcef828f8bc54b6cc2427c1e5f1e01
mdiop <diop.mamadou-pro@laposte.net>
2017-11-24 12:11:06 +0100

    #add librairies imagine php manipulation image

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -28,11 +28,22 @@ class ComposerStaticInit19c961f726446894dae6fa9682b814bf
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'I' => 
+        array (
+            'Imagine' => 
+            array (
+                0 => __DIR__ . '/..' . '/imagine/imagine/lib',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit19c961f726446894dae6fa9682b814bf::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit19c961f726446894dae6fa9682b814bf::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit19c961f726446894dae6fa9682b814bf::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
